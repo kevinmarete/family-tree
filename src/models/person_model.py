a76bc28cb5b88705596956e819096564a14d1d9c
Kevin Marete <kevinmmarete@gmail.com>
2023-06-15 13:30:15 -0400

    Added partners attribute to person model

--- src/models/person_model.py
+++ src/models/person_model.py
@@ -10,16 +10,23 @@ class Person:
         self.date_of_birth: datetime.date = date_of_birth
         self.sex: Sex = sex
         self.parents: dict = {}
+        self.partners: dict = {}
         self.children: dict = {}
 
     def add_parent(self, parent):
         self.parents[parent.id_number] = parent
+
+    def add_partner(self, partner):
+        self.partners[partner.id_number] = partner
 
     def add_child(self, child):
         self.children[child.id_number] = child
 
     def get_parents(self) -> list:
         return list(self.parents.values())
+
+    def get_partners(self) -> list:
+        return list(self.partners.values())
 
     def get_children(self) -> list:
         return list(self.children.values())
