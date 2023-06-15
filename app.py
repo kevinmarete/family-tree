75b72d1719d9d2a394154d12e7c198e3a28e7a74
Kevin Marete <kevinmmarete@gmail.com>
2023-06-15 14:41:24 -0400

    Updated app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
 
 from data.pipeline import CsvPipeline
 from src.models.person_model import Person
@@ -10,41 +12,38 @@ family_service = FamilyService()
 
 def init_data():
     persons = CsvPipeline("data/people.csv").extract().transform().load()
-    relations = CsvPipeline("data/relations.csv").extract().transform().load()
+    parent_relations = CsvPipeline("data/parent_relations.csv").extract().transform().load()
+    partner_relations = CsvPipeline("data/partner_relations.csv").extract().transform().load()
 
     for person in persons:
         new_person = Person(person["id_number"], person["name"], person["date_of_birth"], person["sex"])
         family_service.add_member(new_person)
 
-    for relation in relations:
-        child_member = family_service.get_member(relation["child_id"])
-        parent_member = family_service.get_member(relation["parent_id"])
+    for parent_relation in parent_relations:
+        child_member = family_service.get_member(parent_relation["child_id"])
+        parent_member = family_service.get_member(parent_relation["parent_id"])
 
         child_member.add_parent(parent_member)
         parent_member.add_child(child_member)
 
+    for partner_relation in partner_relations:
+        person_member = family_service.get_member(partner_relation["person_id"])
+        partner_member = family_service.get_member(partner_relation["partner_id"])
+
+        person_member.add_partner(partner_member)
+        partner_member.add_partner(person_member)
+
 
 @app.route('/')
 def welcome():
-    return "Welcome to Family Tree Service"
+    init_data()
+    return render_template('index.html', people=family_service.get_members())
 
 
 @app.route('/tree/<member_id>')
 def get_tree(member_id):
     init_data()
-    member = family_service.get_member(member_id)
-    return {
-        "info": {"id": member.id_number, "name": member.name},
-        "relationships": {
-            "GrandParents": [member.name for member in family_service.get_grand_parents(member_id)],
-            "Parents": [member.name for member in family_service.get_parents(member_id)],
-            "Siblings": [member.name for member in family_service.get_siblings(member_id)],
-            "Uncles": [member.name for member in family_service.get_uncles(member_id)],
-            "Aunts": [member.name for member in family_service.get_aunts(member_id)],
-            "Cousins": [member.name for member in family_service.get_cousins(member_id)],
-            "Ancestors": [member.name for member in family_service.get_ancestors(member_id)]
-        }
-    }
+    return render_template('tree.html', tree=list(family_service.get_family(member_id).values()))
 
 
 if __name__ == '__main__':
